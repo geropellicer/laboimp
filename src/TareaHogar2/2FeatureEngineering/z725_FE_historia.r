8f278e395c84586dcb956ea21f0b3618162f7694
geropellicer <geropellicer@gmail.com>
2022-10-18 21:54:04 -0300

    Saco mis booleanos

--- src/TareaHogar2/2FeatureEngineering/z725_FE_historia.r
+++ src/TareaHogar2/2FeatureEngineering/z725_FE_historia.r
@@ -18,13 +18,13 @@ require("lightgbm")
 
 
 #Parametros del script
-kexperimento <- "FE7250"
+kexperimento <- "FE7257"
 
-kexp_input  <- "DR7140"
+kexp_input  <- "DR7143"
 
 klag1  <- TRUE
 klag2  <- FALSE
-kTendencias  <- FALSE
+kTendencias  <- TRUE
 kRandomForest  <- FALSE
 kCanaritosAsesinos  <- FALSE
 # FIN Parametros del script
@@ -124,23 +124,23 @@ TendenciaYmuchomas  <- function( dataset, cols, ventana=6, tendencia=TRUE, minim
   gc()
   #Esta es la cantidad de meses que utilizo para la historia
   ventana_regresion  <- ventana
-
+  
   last  <- nrow( dataset )
-
+  
   #creo el vector_desde que indica cada ventana
   #de esta forma se acelera el procesamiento ya que lo hago una sola vez
   vector_ids   <- dataset$numero_de_cliente
-
+  
   vector_desde  <- seq( -ventana_regresion+2,  nrow(dataset)-ventana_regresion+1 )
   vector_desde[ 1:ventana_regresion ]  <-  1
-
+  
   for( i in 2:last )  if( vector_ids[ i-1 ] !=  vector_ids[ i ] ) {  vector_desde[i] <-  i }
   for( i in 2:last )  if( vector_desde[i] < vector_desde[i-1] )  {  vector_desde[i] <-  vector_desde[i-1] }
-
+  
   for(  campo  in   cols )
   {
     nueva_col     <- fhistC( dataset[ , get(campo) ], vector_desde ) 
-
+    
     if(tendencia)  dataset[ , paste0( campo, "_tend", ventana) := nueva_col[ (0*last +1):(1*last) ]  ]
     if(minimo)     dataset[ , paste0( campo, "_min", ventana)  := nueva_col[ (1*last +1):(2*last) ]  ]
     if(maximo)     dataset[ , paste0( campo, "_max", ventana)  := nueva_col[ (2*last +1):(3*last) ]  ]
@@ -148,7 +148,7 @@ TendenciaYmuchomas  <- function( dataset, cols, ventana=6, tendencia=TRUE, minim
     if(ratioavg)   dataset[ , paste0( campo, "_ratioavg", ventana)  := get(campo) /nueva_col[ (3*last +1):(4*last) ]  ]
     if(ratiomax)   dataset[ , paste0( campo, "_ratiomax", ventana)  := get(campo) /nueva_col[ (2*last +1):(3*last) ]  ]
   }
-
+  
 }
 #------------------------------------------------------------------------------
 #agrega al dataset nuevas variables {0,1} que provienen de las hojas de un Random Forest
@@ -157,17 +157,46 @@ AgregaVarRandomForest  <- function( num.trees, max.depth, min.node.size, mtry)
 {
   gc()
   dataset[ , clase01:= ifelse( clase_ternaria=="CONTINUA", 0, 1 ) ]
-
+  
   campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria" ) )
-
+  
   dataset_rf  <- copy( dataset[ , campos_buenos, with=FALSE] )
   azar  <- runif( nrow(dataset_rf) )
   dataset_rf[ , entrenamiento := as.integer( foto_mes>= 202009 &  foto_mes<= 202101 & ( clase01==1 | azar < 0.10 )) ]
-
+  
   #imputo los nulos, ya que ranger no acepta nulos
   #Leo Breiman, ¿por que le temias a los nulos?
-  dataset_rf  <- na.roughfix( dataset_rf )
+  dataset_rf[, g_prom_90_mayor_prom_30 := ifelse(g_prom_90_mayor_prom_30, 1, 0)]
+  dataset_rf[, g_esta_complicado := ifelse(g_esta_complicado, 1, 0)]
+  dataset_rf[, g_es_activo_virtual := ifelse(g_es_activo_virtual, 1, 0)]
+  dataset_rf[, g_es_activo_remoto := ifelse(g_es_activo_remoto, 1, 0)]
+  dataset_rf[, g_usa_pago_mis_cuentas := ifelse(g_usa_pago_mis_cuentas, 1, 0)]
+  dataset_rf[, g_tiene_inversiones := ifelse(g_tiene_inversiones, 1, 0)]
+  dataset_rf[, g_posee_prestamos := ifelse(g_posee_prestamos, 1, 0)]
+  dataset_rf[, g_tiene_saldos_negativos := ifelse(g_tiene_saldos_negativos, 1, 0)]
+  dataset_rf[, g_prom_90_mayor_prom_30_lag1 := ifelse(g_prom_90_mayor_prom_30_lag1, 1, 0)]
+  dataset_rf[, g_esta_complicado_lag1 := ifelse(g_esta_complicado_lag1, 1, 0)]
+  dataset_rf[, g_es_activo_virtual_lag1 := ifelse(g_es_activo_virtual_lag1, 1, 0)]
+  dataset_rf[, g_es_activo_remoto_lag1 := ifelse(g_es_activo_remoto_lag1, 1, 0)]
+  dataset_rf[, g_usa_pago_mis_cuentas_lag1 := ifelse(g_usa_pago_mis_cuentas_lag1, 1, 0)]
+  dataset_rf[, g_tiene_inversiones_lag1 := ifelse(g_tiene_inversiones_lag1, 1, 0)]
+  dataset_rf[, g_posee_prestamos_lag1 := ifelse(g_posee_prestamos_lag1, 1, 0)]
+  dataset_rf[, g_tiene_saldos_negativos_lag1 := ifelse(g_tiene_saldos_negativos_lag1, 1, 0)]
+  
+  isfac <- sapply(dataset_rf, is.factor)
+  isnum <- sapply(dataset_rf, is.numeric)
+  
+  j = 0;
+  for (var in isnum){
+    j = j+1;
+    if (var == FALSE){
+      print(isnum[j])
+    }
+  }
+  
 
+  dataset_rf  <- na.roughfix( dataset_rf )
+  
   campos_buenos  <- setdiff( colnames(dataset_rf), c("clase_ternaria","entrenamiento" ) )
   modelo  <- ranger( formula= "clase01 ~ .",
                      data=  dataset_rf[ entrenamiento==1L, campos_buenos, with=FALSE  ] ,
@@ -177,31 +206,31 @@ AgregaVarRandomForest  <- function( num.trees, max.depth, min.node.size, mtry)
                      max.depth=     max.depth,
                      min.node.size= min.node.size,
                      mtry=          mtry
-                   )
-
+  )
+  
   rfhojas  <- predict( object= modelo, 
                        data= dataset_rf[ , campos_buenos, with=FALSE ],
                        predict.all= TRUE,    #entrega la prediccion de cada arbol
                        type= "terminalNodes" #entrega el numero de NODO el arbol
-                     )
-
+  )
+  
   for( arbol in 1:num.trees )
   {
     hojas_arbol  <- unique(  rfhojas$predictions[  , arbol  ] )
-
+    
     for( pos in 1:length(hojas_arbol) )
     {
       nodo_id  <- hojas_arbol[ pos ]  #el numero de nodo de la hoja, estan salteados
       dataset[  ,  paste0( "rf_", sprintf( "%03d", arbol ), "_", sprintf( "%03d", nodo_id ) ) := 0L ]
-
+      
       dataset[ which( rfhojas$predictions[ , arbol] == nodo_id ,  ), 
                paste0( "rf_", sprintf( "%03d", arbol ), "_", sprintf( "%03d", nodo_id ) ) := 1L ]
     }
   }
-
+  
   rm( dataset_rf )
   dataset[ , clase01 := NULL ]
-
+  
   gc()
 }
 #------------------------------------------------------------------------------
@@ -211,19 +240,19 @@ fganancia_lgbm_meseta  <- function(probs, datos)
 {
   vlabels  <- get_field(datos, "label")
   vpesos   <- get_field(datos, "weight")
-
+  
   tbl  <- as.data.table( list( "prob"=probs, "gan"= ifelse( vlabels==1 & vpesos > 1, 78000, -2000 ) ) )
-
+  
   setorder( tbl, -prob )
   tbl[ , posicion := .I ]
   tbl[ , gan_acum :=  cumsum( gan ) ]
   setorder( tbl, -gan_acum )   #voy por la meseta
-
+  
   gan  <- mean( tbl[ 1:500,  gan_acum] )  #meseta de tamaño 500
-
+  
   pos_meseta  <- tbl[ 1:500,  median(posicion)]
   VPOS_CORTE  <<- c( VPOS_CORTE, pos_meseta )
-
+  
   return( list( "name"= "ganancia", 
                 "value"=  gan,
                 "higher_better"= TRUE ) )
@@ -239,27 +268,27 @@ CanaritosAsesinos  <- function( canaritos_ratio=0.2 )
 {
   gc()
   dataset[ , clase01:= ifelse( clase_ternaria=="CONTINUA", 0, 1 ) ]
-
+  
   for( i  in 1:(ncol(dataset)*canaritos_ratio))  dataset[ , paste0("canarito", i ) :=  runif( nrow(dataset))]
-
+  
   campos_buenos  <- setdiff( colnames(dataset), c("clase_ternaria","clase01", "foto_mes" ) )
-
+  
   azar  <- runif( nrow(dataset) )
   dataset[ , entrenamiento := foto_mes>= 202010 &  foto_mes<= 202101  & ( clase01==1 | azar < 0.10 ) ]
-
+  
   dtrain  <- lgb.Dataset( data=    data.matrix(  dataset[ entrenamiento==TRUE, campos_buenos, with=FALSE]),
                           label=   dataset[ entrenamiento==TRUE, clase01],
                           weight=  dataset[ entrenamiento==TRUE, ifelse(clase_ternaria=="BAJA+2", 1.0000001, 1.0)],
                           free_raw_data= FALSE
-                        )
-
+  )
+  
   dvalid  <- lgb.Dataset( data=    data.matrix(  dataset[ foto_mes==202103, campos_buenos, with=FALSE]),
                           label=   dataset[ foto_mes==202103, clase01],
                           weight=  dataset[ foto_mes==202103, ifelse(clase_ternaria=="BAJA+2", 1.0000001, 1.0)],
                           free_raw_data= FALSE
-                          )
-
-
+  )
+  
+  
   param <- list( objective= "binary",
                  metric= "custom",
                  first_metric_only= TRUE,
@@ -279,30 +308,30 @@ CanaritosAsesinos  <- function( canaritos_ratio=0.2 )
                  min_data_in_leaf= 260,
                  num_leaves= 60,
                  early_stopping_rounds= 200 )
-
+  
   modelo  <- lgb.train( data= dtrain,
                         valids= list( valid= dvalid ),
                         eval= fganancia_lgbm_meseta,
                         param= param,
                         verbose= -100 )
-
+  
   tb_importancia  <- lgb.importance( model= modelo )
   tb_importancia[  , pos := .I ]
-
+  
   fwrite( tb_importancia, 
           file= paste0( "impo_", GVEZ ,".txt"),
           sep= "\t" )
-
+  
   GVEZ  <<- GVEZ + 1
-
+  
   umbral  <- tb_importancia[ Feature %like% "canarito", median(pos) + 2*sd(pos) ]  #Atencion corto en la mediana mas DOS desvios!!
-
+  
   col_utiles  <- tb_importancia[ pos < umbral & !( Feature %like% "canarito"),  Feature ]
   col_utiles  <-  unique( c( col_utiles,  c("numero_de_cliente","foto_mes","clase_ternaria","mes") ) )
   col_inutiles  <- setdiff( colnames(dataset), col_utiles )
-
+  
   dataset[  ,  (col_inutiles) := NULL ]
-
+  
 }
 #------------------------------------------------------------------------------
 #agrega para cada columna de cols una nueva variable _rank  que es un numero entre 0 y 1  del ranking de esa variable ese mes
@@ -311,11 +340,11 @@ Rankeador  <- function( cols )
 {
   gc()
   sufijo  <- "_rank" 
-
+  
   for( vcol in cols )
   {
-     dataset[ , paste0( vcol, sufijo) := frank( get(vcol), ties.method= "random")/ .N, 
-                by= foto_mes ]
+    dataset[ , paste0( vcol, sufijo) := frank( get(vcol), ties.method= "random")/ .N, 
+             by= foto_mes ]
   }
 }
 #------------------------------------------------------------------------------
@@ -348,9 +377,9 @@ if( klag1 )
 {
   #creo los campos lags de orden 1
   dataset[ , paste0( cols_lagueables, "_lag1") := shift(.SD, 1, NA, "lag"), 
-             by= numero_de_cliente, 
-             .SDcols= cols_lagueables ]
-
+           by= numero_de_cliente, 
+           .SDcols= cols_lagueables ]
+  
   #agrego los delta lags de orden 1
   for( vcol in cols_lagueables )
   {
@@ -363,9 +392,9 @@ if( klag2 )
 {
   #creo los campos lags de orden 2
   dataset[ , paste0( cols_lagueables, "_lag2") := shift(.SD, 2, NA, "lag"), 
-             by= numero_de_cliente, 
-             .SDcols= cols_lagueables ]
-
+           by= numero_de_cliente, 
+           .SDcols= cols_lagueables ]
+  
   #agrego los delta lags de orden 2
   for( vcol in cols_lagueables )
   {
@@ -403,7 +432,7 @@ if( kRandomForest )
                          max.depth = 5,
                          min.node.size = 500,
                          mtry = 15 )
-
+  
   gc()
 }
 
